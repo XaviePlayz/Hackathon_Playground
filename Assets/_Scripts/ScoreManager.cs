923ad7c19375c4bc7c3477be978c243229f6d07f
Xavier Wevers <xavier.wevers@lucky-kat.com>
2022-11-29 11:32:54 +0100

    hotfix pointsystem

--- Assets/_Scripts/ScoreManager.cs
+++ Assets/_Scripts/ScoreManager.cs
@@ -5,16 +5,9 @@ using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
-    public static ScoreManager instance; 
     public Text scoreText;
 
     public int score = 0;
-
-    // Start is called before the first frame update
-    void Awake()
-    {
-        instance = this;
-    }
 
     void Start()
     {

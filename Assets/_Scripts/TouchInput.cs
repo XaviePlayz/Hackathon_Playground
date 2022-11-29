6919bab8cd5e05dd6c507a9a1ede45e3e6dd993c
Xavier Wevers <xavier.wevers@lucky-kat.com>
2022-11-29 11:03:19 +0100

    Trying to Destroy object

--- Assets/_Scripts/TouchInput.cs
+++ Assets/_Scripts/TouchInput.cs
@@ -1,62 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.XR.ARFoundation;
-using UnityEngine.XR.ARSubsystems;
 
-[RequireComponent(typeof(ARRaycastManager))]
 public class TouchInput : MonoBehaviour
 {
-    public GameObject gameObjectToInstantiate;
-
-    private GameObject spawnedObject;
-    private ARRaycastManager _arRaycastManager;
-    private Vector2 touchPosition;
-
-    static List<ARRaycastHit> hits = new List<ARRaycastHit>();
-
-    private void Awake()
-    {
-        _arRaycastManager = GetComponent<ARRaycastManager>();
-    }
-
-    bool TryGetTouchPosition(out Vector2 touchPosition)
-    {
-        if (Input.touchCount > 0)
-        {
-            touchPosition = Input.GetTouch(index:0).position;
-            return true;
-        }
-
-        touchPosition = default;
-        return false;
-    }
+    public GameObject cube;
 
     void Update()
     {
-        if (!TryGetTouchPosition(out Vector2 touchPostion))
-            return;
-
-        if (_arRaycastManager.Raycast(touchPostion, hits, trackableTypes: TrackableType.PlaneWithinPolygon))
+        if (Input.touchCount > 0)
         {
-            var hitPose = hits[0].pose;
-
-            if (spawnedObject == null)
-            {
-                spawnedObject = Instantiate(gameObjectToInstantiate, hitPose.position, hitPose.rotation);
-            }
-            else
-            {
-                spawnedObject.transform.position = hitPose.position;
-            }
-        }
-    }
-
-    private void OnTriggerEnter(Collider other)
-    {
-        if (other.tag == "Object")
-        {
-            Destroy(gameObject);
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.GetTouch(0).position);
+            if (Physics.Raycast(ray, out hit))
+                if (hit.collider.gameObject.tag == "collectibleToDestroy")
+                {
+                    Destroy(cube);
+                }
         }
     }
 }

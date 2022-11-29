bf8798333735508cf2de98d8c62e16b449ad2d25
Xavier Wevers <xavier.wevers@lucky-kat.com>
2022-11-29 13:30:21 +0100

    Generate system

---
+++ Assets/_Scripts/GenerateObject.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GenerateObject : MonoBehaviour
+{
+    public Timer time;
+
+    public GameObject[] spawners;
+    public GameObject[] objects;
+
+    float timeDelay;
+    float objectGenerator;
+
+    public void Awake()
+    {
+        objectGenerator = Random.Range(1, 3);
+
+    }
+    public void Start()
+    {
+        objects = new GameObject[1];
+        spawners = new GameObject[5];
+
+        for (int i = 0; i < objects.Length; i++)
+        {
+            objects[i] = transform.GetChild(i).gameObject;
+        }
+
+        for (int i = 0; i < spawners.Length; i++)
+        {
+            spawners[i] = transform.GetChild(i).gameObject;
+        }
+        StartCoroutine(InstantiateObject());
+    }
+
+    IEnumerator InstantiateObject()
+    {
+        timeDelay = Random.Range(0.2f, 2f);
+        yield return new WaitForSeconds(timeDelay);
+
+        if (time.timeValue > 0)
+        {
+            int spawnerID = Random.Range(2, 6);
+            int creatureID = Random.Range(0, objects.Length);
+
+            Instantiate(objects[creatureID], spawners[spawnerID].transform.position, spawners[spawnerID].transform.rotation);
+            StartCoroutine(InstantiateObject());
+        }
+    }
+}
